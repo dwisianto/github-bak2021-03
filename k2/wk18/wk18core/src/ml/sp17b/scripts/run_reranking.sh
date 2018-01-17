02f6ebe2e234111694a689f30f9d3bed453a006e
dwisianto <dwisianto@gmail.com>
2018-01-17 16:10:19 -0500

    clean up

--- k2/wk18/wk18core/src/ml/sp17b/scripts/run_reranking.sh
+++
@@ -1,4 +0,0 @@
-JAVA=java
-jarPath=dist/illinois-sl-1.3-jar-with-dependencies.jar
-
-nice $JAVA  -ea -Xmx4096M -cp $jarPath edu.illinois.cs.cogcomp.sl.applications.reranking.MainClass  $*
--- k2/wk18/wk18core/src/ml/sp17b/scripts/run_tutorial.sh
+++
@@ -1,4 +0,0 @@
-JAVA=java
-jarPath=dist/illinois-sl-1.3-jar-with-dependencies.jar
-
-nice $JAVA  -ea -Xmx2096M -cp $jarPath edu.illinois.cs.cogcomp.sl.applications.tutorial.MainClass  $*
